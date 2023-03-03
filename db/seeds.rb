b397bbeb706c75ff57d48110437388c5dbf3f314
ifedan <danielonyejesi@gmail.com>
2023-03-03 11:29:18 +0100

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create!(title: 'Biscuits for My Dear One',
+  description:
+    %{<p>
+      <em>Lorem Ipsum</em>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ipsum arcu, pharetra non vehicula eget, malesuada non nunc. Maecenas libero ante, pharetra sit amet nulla eget, euismod luctus magna. Mauris lacinia nibh rhoncus, tincidunt augue nec, dignissim tellus. Vivamus pellentesque mi vitae feugiat semper. Donec eget vulputate elit. Nulla commodo, sem in pellentesque euismod, quam justo rutrum nulla, quis lobortis tortor urna non nisl. Vestibulum et lacus vestibulum, iaculis leo a, convallis dolor. Maecenas ac tincidunt ipsum, ac faucibus augue. Aenean ut feugiat arcu. Vestibulum consequat lectus justo, id aliquam nisi congue eget. Donec eu sagittis urna. Nunc quis nulla quam. Aenean quis sapien et mi finibus tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+      </p>},
+  image_url: 'biscuits.jpg',
+  price: 10.00)
+# . . .
+Product.create!(title: 'Anything for My Very Best',
+  description:
+    %{<p>
+      <em>Lorem ipsum</em>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ipsum arcu, pharetra non vehicula eget, malesuada non nunc. Maecenas libero ante, pharetra sit amet nulla eget, euismod luctus magna. Mauris lacinia nibh rhoncus, tincidunt augue nec, dignissim tellus. Vivamus pellentesque mi vitae feugiat semper. Donec eget vulputate elit. Nulla commodo, sem in pellentesque euismod, quam justo rutrum nulla, quis lobortis tortor urna non nisl. Vestibulum et lacus vestibulum, iaculis leo a, convallis dolor. Maecenas ac tincidunt ipsum, ac faucibus augue. Aenean ut feugiat arcu. Vestibulum consequat lectus justo, id aliquam nisi congue eget. Donec eu sagittis urna. Nunc quis nulla quam. Aenean quis sapien et mi finibus tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+      </p>},
+  image_url: 'egusi_soup.jpg',
+  price: 20.00)
+# . . .
+
+Product.create!(title: 'The Book of Life',
+  description:
+    %{<p>
+      <em>Lorem Ipsum</em>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ipsum arcu, pharetra non vehicula eget, malesuada non nunc. Maecenas libero ante, pharetra sit amet nulla eget, euismod luctus magna. Mauris lacinia nibh rhoncus, tincidunt augue nec, dignissim tellus. Vivamus pellentesque mi vitae feugiat semper. Donec eget vulputate elit. Nulla commodo, sem in pellentesque euismod, quam justo rutrum nulla, quis lobortis tortor urna non nisl. Vestibulum et lacus vestibulum, iaculis leo a, convallis dolor. Maecenas ac tincidunt ipsum, ac faucibus augue. Aenean ut feugiat arcu. Vestibulum consequat lectus justo, id aliquam nisi congue eget. Donec eu sagittis urna. Nunc quis nulla quam. Aenean quis sapien et mi finibus tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+      </p>},
+  image_url: 'bible.jpg',
+  price: 40.00)
+
+  Product.create!(title: 'Biscuits for My Dear One',
+    description:
+      %{<p>
+        <em>Lorem Ipsum</em>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ipsum arcu, pharetra non vehicula eget, malesuada non nunc. Maecenas libero ante, pharetra sit amet nulla eget, euismod luctus magna. Mauris lacinia nibh rhoncus, tincidunt augue nec, dignissim tellus. Vivamus pellentesque mi vitae feugiat semper. Donec eget vulputate elit. Nulla commodo, sem in pellentesque euismod, quam justo rutrum nulla, quis lobortis tortor urna non nisl. Vestibulum et lacus vestibulum, iaculis leo a, convallis dolor. Maecenas ac tincidunt ipsum, ac faucibus augue. Aenean ut feugiat arcu. Vestibulum consequat lectus justo, id aliquam nisi congue eget. Donec eu sagittis urna. Nunc quis nulla quam. Aenean quis sapien et mi finibus tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        </p>},
+    image_url: 'biscuits.jpg',
+    price: 10.00)
+  # . . .
+  Product.create!(title: 'Anything for My Very Best',
+    description:
+      %{<p>
+        <em>Lorem ipsum</em>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ipsum arcu, pharetra non vehicula eget, malesuada non nunc. Maecenas libero ante, pharetra sit amet nulla eget, euismod luctus magna. Mauris lacinia nibh rhoncus, tincidunt augue nec, dignissim tellus. Vivamus pellentesque mi vitae feugiat semper. Donec eget vulputate elit. Nulla commodo, sem in pellentesque euismod, quam justo rutrum nulla, quis lobortis tortor urna non nisl. Vestibulum et lacus vestibulum, iaculis leo a, convallis dolor. Maecenas ac tincidunt ipsum, ac faucibus augue. Aenean ut feugiat arcu. Vestibulum consequat lectus justo, id aliquam nisi congue eget. Donec eu sagittis urna. Nunc quis nulla quam. Aenean quis sapien et mi finibus tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        </p>},
+    image_url: 'egusi_soup.jpg',
+    price: 20.00)
+  # . . .
+
+  Product.create!(title: 'The Book of Life',
+    description:
+      %{<p>
+        <em>Lorem Ipsum</em>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ipsum arcu, pharetra non vehicula eget, malesuada non nunc. Maecenas libero ante, pharetra sit amet nulla eget, euismod luctus magna. Mauris lacinia nibh rhoncus, tincidunt augue nec, dignissim tellus. Vivamus pellentesque mi vitae feugiat semper. Donec eget vulputate elit. Nulla commodo, sem in pellentesque euismod, quam justo rutrum nulla, quis lobortis tortor urna non nisl. Vestibulum et lacus vestibulum, iaculis leo a, convallis dolor. Maecenas ac tincidunt ipsum, ac faucibus augue. Aenean ut feugiat arcu. Vestibulum consequat lectus justo, id aliquam nisi congue eget. Donec eu sagittis urna. Nunc quis nulla quam. Aenean quis sapien et mi finibus tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        </p>},
+    image_url: 'bible.jpg',
+    price: 40.00)
+
